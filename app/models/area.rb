6d92c857b0b3ef0e6d3e7f59eddf45747e712fb6
Jonthan Pettersson <jonathan.anders.pettersson@gmail.com>
2012-11-01 19:22:01 -0400

    Added some method stubs to Area

--- app/models/area.rb
+++ app/models/area.rb
@@ -2,4 +2,16 @@ class Area < ActiveRecord::Base
   has_ancestry
   has_many :area_samples
   attr_accessible :parent, :area_name, :created_at, :latitude, :longitude, :parent_area_id, :updated_at
+
+  def outage_percentage
+  	area_samples.last.custs_out / area_samples.last.total_custs
+  end
+
+  def max_historic_outage
+
+  end
+
+  def min_historic_outage
+
+  end
 end
--- db/migrate/20121101224605_create_areas.rb
+++ db/migrate/20121101224605_create_areas.rb
@@ -2,7 +2,6 @@ class CreateAreas < ActiveRecord::Migration
   def change
     create_table :areas do |t|
       t.string :area_name
-      t.integer :parent_area_id
       t.string :latitude
       t.string :longitude
       t.datetime :created_at
