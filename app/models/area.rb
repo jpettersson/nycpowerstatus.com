6b0733f921af2ba9038937613fc595326fdf4b1b
Jonthan Pettersson <jonathan.anders.pettersson@gmail.com>
2012-11-01 20:22:35 -0400

    created at method in Area for fetching new samples

--- app/models/area.rb
+++ app/models/area.rb
@@ -6,6 +6,23 @@ class Area < ActiveRecord::Base
   extend FriendlyId
   friendly_id :area_name, use: :slugged
 
+  def self.sample!
+    require 'coned'
+    c = Coned.new
+    c.fetch
+
+    c.data.areas.each {|b| 
+      puts "Burough: #{b.area_name}: #{b.custs_out}/#{b.total_custs} out/total"
+      area = Area.find_by_area_name(b.area_name)
+      AreaSample.create!({:area_id => area.id, :total_custs => b.total_custs, :custs_out => b.custs_out, :etr => b.etr, :etrmillis => b.etrmillis})
+      b.areas.each {|n|
+        puts "  Neighborhood: #{n.area_name} #{n.custs_out}/#{n.total_custs} out/total"
+        sub_area = Area.find_by_area_name(b.area_name)
+        AreaSample.create!({:area_id => sub_area.id, :total_custs => n.total_custs, :custs_out => n.custs_out, :etr => n.etr, :etrmillis => n.etrmillis})
+      }
+    } 
+  end
+
   def self.root_outage_percentage
     areas = Area.at_depth(0)
     total = areas.map{|a| a.area_samples.last.total_custs}
