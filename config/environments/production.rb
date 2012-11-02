8e2dd1a92927d01a6fd4303f0b9f9e27a363ee99
Jonthan Pettersson <jonathan.anders.pettersson@gmail.com>
2012-11-02 00:14:10 -0400

    Serving static assets w rails for dev

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Doesnychavepower::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   config.assets.compile = true
+  config.serve_static_assets = true
   # Code is not reloaded between requests
   config.cache_classes = true
 
