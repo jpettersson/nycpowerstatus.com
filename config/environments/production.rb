3958c795543dc64c3578d1a5c22f117289284c0b
Jonthan Pettersson <jonathan.anders.pettersson@gmail.com>
2012-11-02 00:15:33 -0400

    Using rails for static assets for dev

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Doesnychavepower::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   config.assets.compile = true
   config.serve_static_assets = true
+  
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -9,14 +10,8 @@ Doesnychavepower::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-
   # Compress JavaScripts and CSS
   config.assets.compress = true
-
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
