f5642724df066ab24d3934b1d14405f8eecb96c4
Jonthan Pettersson <jonathan.anders.pettersson@gmail.com>
2012-11-01 23:30:36 -0400

    footer and some credits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   before_filter :get_footer_data
 
   def get_footer_data
-    @updated_at = AreaSample.last.updated_at
+    @updated_at = AreaSample.last.updated_at.in_time_zone("Eastern Time (US & Canada)").strftime("%m/%d/%Y %H:%M EDT")
   end
 end
