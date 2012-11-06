422df6d436b8022c3d374af99cda5f2bf7a43947
Jonthan Pettersson <jonathan.anders.pettersson@gmail.com>
2012-11-06 01:05:12 -0500

    Temporarily filtering out areas tagged as hidden

--- app/controllers/power_controller.rb
+++ app/controllers/power_controller.rb
@@ -1,7 +1,7 @@
 class PowerController < ApplicationController
   def index
     @provider = Provider.find_by_code(params[:provider])
-    @areas = @provider.areas.at_depth(0)
+    @areas = @provider.areas.at_depth(0).reject{|a| a.is_hidden }
     @total_outage = @provider.outage_percentage
     num = (@total_outage * 100).to_s.split(".")
     @pretty_outage_percent = "#{num[0]}.#{num[1][0..1]}"
@@ -19,7 +19,7 @@ class PowerController < ApplicationController
     @pretty_outage_percent = "#{num[0]}.#{num[1][0..1]}"
 
     if @area.children.length > 0
-      @map_points_json = create_map_points @area.children
+      @map_points_json = create_map_points @area.children.reject{|a| a.is_hidden }
       @time_series_json = create_time_series [@area]
     else
       @map_points_json = create_map_points [@area]
