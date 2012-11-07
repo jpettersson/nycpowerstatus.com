2e40f5227e011d645f834df0ef25f690b7d177cd
Jonthan Pettersson <jonathan.anders.pettersson@gmail.com>
2012-11-07 02:48:44 -0500

    added PSEG to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,21 @@
 # Create the supported providers.
 [ 
   {:code => 'ConEd', :name => "Con Edison", :provider_type => 'electricity', :url => 'http://www.conedison.com/', :longitude => '', :latitude => '', :slug => ''},
-  {:code => 'LIPA', :name => "Long Island Power Authority", :provider_type => 'electricity', :longitude => '40.784909', :latitude => '-73.381805', :url => 'http://www.lipower.org/', :slug => 'long-island'}
+  {:code => 'LIPA', :name => "Long Island Power Authority", :provider_type => 'electricity', :longitude => '-73.381805', :latitude => '40.784909', :url => 'http://www.lipower.org/', :slug => 'long-island'},
+  {:code => 'PSEG', :name => "Public Service Enterprise Group", :provider_type => 'electricity', :longitude => '-74.233246', :latitude => '40.702609', :url => 'http://www.pseg.com/', :slug => 'new-jersey'}
 ].each do |p|
     unless provider = Provider.find_by_name(p[:name])
        provider = Provider.create!(p)
     end
 end
 
-# Update the existing areas with the ConEd Provider.id
-provider = Provider.find_by_code "ConEd"
-Area.all.each do |area|
-  provider.areas << area
-end
-
 # Sample some data.
-Provider.all.each do |provider|
-  begin
-    provider.sample! true
-  rescue Exception => msg
-    puts msg
-  end
-end
+# Provider.all.each do |provider|
+#   begin
+#     provider.sample! true
+#   rescue Exception => msg
+#     puts msg
+#   end
+# end
 
 # rake "geocode"
