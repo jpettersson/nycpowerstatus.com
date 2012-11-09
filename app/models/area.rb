ddd5304275561532f4fd95b57cde607721ba1a06
Jonthan Pettersson <jonathan.anders.pettersson@gmail.com>
2012-11-09 01:15:16 -0500

    Fixed a few mistakes with single providers in index

--- app/models/area.rb
+++ app/models/area.rb
@@ -7,42 +7,8 @@ class Area < ActiveRecord::Base
   extend FriendlyId
   friendly_id :area_name, use: :slugged
 
-  def self.sample! force=false
-    require 'coned'
-    c = Coned.new
-    c.fetch
-
-    do_sample = ->(area, raw){
-      AreaSample.create!({:area_id => area.id, :total_custs => raw.total_custs, :custs_out => raw.custs_out, :etr => raw.etr, :etrmillis => raw.etrmillis})
-    }
-
-    # Only create a new sample if coned has updated the values.
-    maybe_sample = ->(area, raw){
-      unless force
-        last = area.samples.last
-        unless last.nil?
-          unless last.total_custs == raw.total_custs and last.custs_out == raw.custs_out and last.etr == raw.etr
-            puts "sample: #{raw.area_name.to_s} #{raw.custs_out.to_s} / #{raw.total_custs.to_s}"
-            do_sample.call(area, raw)
-          end
-        end
-      else
-        do_sample.call(area, raw)
-      end
-    }
-
-    puts 'start sampling'
-    puts Time.new.inspect
-    c.data.areas.each {|b| 
-      area = Area.find_by_area_name(b.area_name)
-      maybe_sample.call(area, b)
-
-      b.areas.each {|n|
-        sub_area = Area.find_by_area_name(n.area_name)
-        maybe_sample.call(sub_area, n)
-      }
-    } 
-    puts 'end sampling'
+  def name
+    area_name
   end
 
   def last_sample
